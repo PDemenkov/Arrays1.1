5afe2c7b94bf2913147040dc19f8efaf471dc0a9
PDemenkov <guapnik@gmail.com>
2022-09-26 16:54:49 +0300

    Дз выполнено 4 задачи +1 for расширенный

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int[] arrray = new int[3];
-        arrray[0] = 1;
-        arrray[1] = 2;
-        arrray[2] = 3;
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
 
         double[] array1 = {1.57, 7.654, 9.986};
 
@@ -13,9 +15,19 @@ public class Main {
 
 
         System.out.println("Задание 2");
-        for (int i = 0; i < arrray.length; i++) {
-            System.out.print(arrray[i] + ", ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
+        for (int x : array){
+            System.out.print(x);
+            System.out.print(",");
+        }
+
+
         System.out.println();
         for (int a = 0; a < array1.length; a++) {
             System.out.print(array1[a] + ", ");
@@ -27,8 +39,8 @@ public class Main {
         System.out.println();
 
         System.out.println("Задание 3");
-        for (int i = arrray.length - 1; i >= 0; i--) {
-            System.out.print(arrray[i] + ", ");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + ", ");
         }
         System.out.println();
         for (int i = array1.length - 1; i >= 0; i--) {
@@ -41,11 +53,10 @@ public class Main {
         System.out.println();
         System.out.println("Задание 4");
 
-        for (int i = 0; i < array2.length; i++) {
-            if (array2[i] % 2 != 0) {
-                array2[i]++;
-                System.out.println("Index: " + i + " number " + array2[i] );
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i]+=1;
             }
-        }
+        } System.out.println(Arrays.toString(array));
     }
 }
